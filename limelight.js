38607ebf868c4df3fe35aacb1bfc4da8811200ec
Luke Watts <luke@thisis.la>
2017-08-08 14:52:36 +0100

    🎨Prettify js

--- limelight.js
+++ limelight.js
@@ -1,34 +1,43 @@
-/* global jQuery */
-'use strict';
+/* global jQuery, document */
+"use strict";
 
 var limeLightItems = [];
 
 var LimeLight = function($el) {
-	this.$el = $el;
+  this.$el = $el;
 
-	this.positionX = this.$el.offset().left;
-	this.positionY = this.$el.offset().top;
+  this.positionX = this.$el.offset().left;
+  this.positionY = this.$el.offset().top;
 
-	return this;
+  // Debugging
+  //   document.body.append(
+  //     '<div style="position: fixed; top: ' + this.positionX + '">Text</div>'
+  //   );
+
+  return this;
 };
 
-jQuery('[data-limelight]').each(function() {
-	limeLightItems.push(new LimeLight(jQuery(this)));
+LimeLight.prototype.updatePosition = function(argument) {
+  this.positionX = this.$el.offset().left;
+  this.positionY = this.$el.offset().top;
+};
+
+jQuery("[data-limelight]").each(function() {
+  limeLightItems.push(new LimeLight(jQuery(this)));
 });
 
 function LimeLightEval() {
+  for (var i = limeLightItems.length - 1; i >= 0; i--) {
+    var item = limeLightItems[i];
 
-	for (var i = limeLightItems.length - 1; i >= 0; i--) {
-		var item = limeLightItems[i];
+    if (document.body.scrollTop + window.innerHeight * 0.75 > item.positionY) {
+      item.$el.addClass("ll__focus");
+    } else {
+      item.$el.removeClass("ll__focus");
+    }
+  }
 
-		if ( document.body.scrollTop + window.innerHeight * 0.75 > item.positionY ) {
-			item.$el.addClass('ll__focus');
-		} else {
-			item.$el.removeClass('ll__focus');
-		}
-	}
-
-	window.requestAnimationFrame(LimeLightEval);
+  window.requestAnimationFrame(LimeLightEval);
 }
 
 LimeLightEval();
